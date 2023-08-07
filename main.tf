a5c1d6a5570f1523e3d4026758de2f5b496b9749
Prerit Chandok <22628474+pc-000@users.noreply.github.com>
2023-08-07 15:17:27 -0400

    change to the pre-made module

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   //Give below variable a list of security groups you want to apply to this instance
-  // vpc_security_group_ids = [aws_security_group.blog.id]
+  //vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
 
@@ -36,11 +36,11 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name    = "blog_sg"
-  vpc_id             = data.aws_vpc.default.id
-  ingress_rules      = ["https-443-tcp"]
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rules       = ["https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules      = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
