123dd5fc286502d90ca8850a2300466f5abe916e
Prerit Chandok <22628474+pc-000@users.noreply.github.com>
2023-08-07 19:56:44 -0400

    create dev vpc

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "blog_sg" {
   
   //vpc_id = data.aws_vpc.default.id
 
-  vpc.id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
