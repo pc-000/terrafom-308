6e01de2a84870c6f6a3bd1b5c5c6d77862d9fdc0
Prerit Chandok <22628474+pc-000@users.noreply.github.com>
2023-08-14 23:19:06 -0400

    security group

--- main.tf
+++ main.tf
@@ -14,7 +14,11 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
 
+/*
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
@@ -30,6 +34,7 @@ module "vpc" {
     Environment = "dev"
   }
 }
+*/
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -50,10 +55,10 @@ module "blog_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   
-//vpc_id = data.aws_vpc.default.id
+vpc_id = data.aws_vpc.default.id
 
   name    = "blog_new"
-  vpc_id = module.vpc.vpc_id
+  //vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
